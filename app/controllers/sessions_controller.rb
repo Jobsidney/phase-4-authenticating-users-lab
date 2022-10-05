7d10c3edd65d42d76f5b56f4251c938dfb50b251
Jobsidney <jobsidney67@gmail.com>
2022-10-05 08:22:28 +0300

    commit tree 75db44e61bf49722fef6f847c49b50a216a2a53c

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
     def create
         user=User.find_by(username: params[:username])
         session[:user_id]=user.id
-
         render json:user
+
     end
 
     def show
